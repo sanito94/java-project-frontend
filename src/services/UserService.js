f331ec806ceee60a84230575200a0b44702d78c7
sanito94 <122975967+sanito94@users.noreply.github.com>
2024-09-26 00:13:40 +0300

    Header added to axios

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -2,26 +2,43 @@ import axios from 'axios';
 
 
 const USER_BASE_REST_API_URL = "http://localhost:9090/users";
+var username = 'admin';
+var password = 'admin123';
+var basicAuth = 'Basic ' + btoa(username + ':' + password);
+var headers = {
+    'Authorization': basicAuth,
+    "Access-Control-Allow-Origin": "*"
+}
 
 class UserService {
-    getAllUsers(){
-        return axios.get(USER_BASE_REST_API_URL);
+    getAllUsers() {
+        return axios.get(USER_BASE_REST_API_URL, {
+            headers: headers
+        });
     }
 
     createUser(user) {
-        return axios.post(USER_BASE_REST_API_URL, user);
+        return axios.post(USER_BASE_REST_API_URL, user, {
+            headers: headers
+        });
     }
 
     getUserById(userId) {
-        return axios.get(USER_BASE_REST_API_URL + "/" + userId);
+        return axios.get(USER_BASE_REST_API_URL + "/" + userId, {
+            headers: headers
+        });
     }
 
     updateUser(userId, user) {
-        return axios.put(USER_BASE_REST_API_URL + "/" + userId, user);
+        return axios.put(USER_BASE_REST_API_URL + "/" + userId, user, {
+            headers: headers
+        });
     }
 
     deleteUser(userId) {
-        return axios.delete(USER_BASE_REST_API_URL + "/" + userId)
+        return axios.delete(USER_BASE_REST_API_URL + "/" + userId, {
+            headers: headers
+        })
     }
 }
 
